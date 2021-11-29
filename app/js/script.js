6e9be96fd14e5112f1e330a85e8dde9965effd92
Peron <tommypriambodo@gmail.com>
2021-11-29 16:50:36 +0700

    new commit

--- app/js/script.js
+++ app/js/script.js
@@ -1,41 +1,18 @@
-var toggle = document.getElementById("btn-toggle");
-var navMobile = document.getElementById("navbarMobile");
+var btnToggle = $("#btn-toggle");
+var btnSearch = $("#search");
 
-function hasClass(elem, className) {
-    return new RegExp(" " + className + " ").test(" " + elem.className + " ");
-}
-// addClass
-function addClass(elem, className) {
-    if (!hasClass(elem, className)) {
-        elem.className += " " + className;
-    }
-}
-// removeClass
-function removeClass(elem, className) {
-    var newClass = " " + elem.className.replace(/[\t\r\n]/g, " ") + " ";
-    if (hasClass(elem, className)) {
-        while (newClass.indexOf(" " + className + " ") >= 0) {
-            newClass = newClass.replace(" " + className + " ", " ");
-        }
-        elem.className = newClass.replace(/^\s+|\s+$/g, "");
-    }
-}
-// toggleClass
-function toggleClass(elem, className) {
-    var newClass = " " + elem.className.replace(/[\t\r\n]/g, " ") + " ";
-    if (hasClass(elem, className)) {
-        while (newClass.indexOf(" " + className + " ") >= 0) {
-            newClass = newClass.replace(" " + className + " ", " ");
-        }
-        elem.className = newClass.replace(/^\s+|\s+$/g, "");
-        navMobile.classList.remove("show");
-    } else {
-        elem.className += " " + className;
-        navMobile.classList.add("show");
-    }
-}
+btnToggle.click(function () {
+    $(this).toggleClass("on");
 
-toggle.onclick = function () {
-    toggleClass(this, "on");
-    return false;
-};
+    if (btnSearch.hasClass("on")) {
+        btnSearch.removeClass("on");
+    }
+});
+
+btnSearch.click(function () {
+    $(this).toggleClass("on");
+
+    if (btnToggle.hasClass("on")) {
+        btnToggle.removeClass("on");
+    }
+});
--- readme.md
+++ readme.md
@@ -5,3 +5,21 @@
 -   light navy blue : #3A6B85
 -   orange : #FFD700
 -   light grey : #EAEAEA
+
+# Font
+
+-   [Roboto](https://fonts.google.com/specimen/Roboto)
+    -   300
+    -   400
+    -   500
+
+# Quote
+
+-   "Doesn't matter who you're born to… Everyone's still a child of the sea!!" ~ Whitebeard
+-   "When do you think people die? When they are shot in the heart with a pistol? No. When they are ravaged by uncurable disease? No. When they drink a soup made from poisonous mushrooms? No!. It’s when… they are forgotten!" ~ Dr. Hiriluk
+-   "When the world shoves you around, you just gotta stand up and shove back. It's not like somebody's gonna save you if you start babbling excuses." ~ Roronoa Zoro
+-   "No matter how hard or how impossible it is, never lose sight of your goal." ~ Monkey D. Luffy
+-   "It may be hard right now… But you must silence those thoughts! Stop counting only those things that you have lost! What is gone, is gone." ~ Jinbei
+-   "Destiny... fate... dreams... These unstoppable ideals are held deep in the heart of man. As long as there are people who seek freedom in this life, these things will not vanish from the earth." ~ Gol D. Roger
+-   "There can be no happiness in a world where the undesirables are thrown away." ~ Monkey D. Dragon
+-   "Life is like a pencil that will surely run out, but will leave the beautiful writing of life." ~ Nami
